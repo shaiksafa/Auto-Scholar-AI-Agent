18e7dc5cf379bb212b3f36ffea52871128d2b50d
Shaik Safa <shaiksafakv@gmail.com>
2026-07-18 11:20:28 +0530

    Fix Cloud Run deployment

--- backend/app.py
+++ backend/app.py
@@ -259,10 +259,10 @@ def download_pdf():
 
 if __name__ == "__main__":
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
 
     app.run(
         host="0.0.0.0",
         port=port,
-        debug=True
+        debug=False
     )
